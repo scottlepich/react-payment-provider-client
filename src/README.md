52369efe52608e4163ec71941b233edf220ca3a3
Scott Lepich <slepich@opentable.com>
2023-07-28 16:21:46 -0700

    stub initial hook

---
+++ src/README.md
@@ -0,0 +1,4 @@
+# @opentable/react-payment-provider-client
+
+TODO:
+Abstraction for payment provider strageties in use in react apps @opentable.
---
+++ src/usePaymentProvider.ts
@@ -0,0 +1,7 @@
+// TODO: return hooks for payment provider integration
+
+interface UsePaymentProvider {}
+
+type ReturnType = {};
+
+export const usePaymentProvider: UsePaymentProvider = (): ReturnType => ({});
