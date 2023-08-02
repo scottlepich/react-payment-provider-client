ab222a00263e2c400b798f2d9968c2bdeb89cc9c
Scott Lepich <slepich@opentable.com>
2023-08-02 15:22:25 -0700

    cleaner

--- src/usePaymentProvider.ts
+++ src/usePaymentProvider.ts
@@ -4,13 +4,14 @@ import useSpreedly from "~spreedly/useSpreedly";
 
 export enum PaymentProviderNames {
   Spreedly = "spreedly",
-  // TODO: Stripe: "stripe";
+  // TODO: Stripe: "stripe",
 }
 
 interface UsePaymentProvider {
   name: PaymentProviderNames;
 }
 
+// return provider hook and context
 type ReturnType = {
   usePayment: any;
   PaymentProvider: any;
@@ -19,19 +20,21 @@ type ReturnType = {
 export const usePaymentProvider = ({
   name,
 }: UsePaymentProvider): ReturnType => {
-  if (name === PaymentProviderNames.Spreedly) {
-    // TODO: move here
-    // Script attach
-    // env key
-
-    const [usePayment, PaymentProvider] = createSingletonHook(useSpreedly);
-
-    return {
-      usePayment,
-      PaymentProvider,
-    };
-    // TODO: elseif stripe
-  } else {
+  // TODO: elseif stripe
+  if (name !== PaymentProviderNames.Spreedly) {
     throw new Error("Unknown PaymentProviderName passed to usePaymentProvider");
   }
+
+  // Spreedly
+  // TODO: move here
+  // Script src loader
+  // env key
+  // state machine to top
+
+  const [usePayment, PaymentProvider] = createSingletonHook(useSpreedly);
+
+  return {
+    usePayment,
+    PaymentProvider,
+  };
 };
