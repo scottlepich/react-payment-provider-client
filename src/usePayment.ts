e0360b73e1e5f55f73cc91d3c6683f0f0c5a25ab
Scott Lepich <slepich@opentable.com>
2023-08-03 16:41:11 -0700

    WIP separate useProviderScript

--- src/usePayment.ts
+++ src/usePayment.ts
@@ -1,41 +1,17 @@
-import { useContext, useEffect } from "react";
-import useScript from "react-script-hook";
-
-import { ActionTypes } from "./types/index.d";
+import { useContext } from "react";
 
 import PaymentContext from "./PaymentContext";
 
-import { PaymentType } from "./constants";
+import useProviderScript from "./useProviderScript";
 
-const { SET_SRC_LOADED } = ActionTypes;
+import { PaymentType } from "./constants";
 
 // TODO: Stripe
 
 const usePayment = (paymentType: PaymentType) => {
   const context = useContext(PaymentContext);
 
-  // TODO: useDispatch
-  if (!context?.dispatch) {
-    return;
-  }
-
-  // Attach provider script.
-  const [scriptLoading] = useScript({
-    src: "", // TODO: use paymentType for src
-    checkForExisting: true, // prevent multiple script injection
-  });
-
-  useEffect(() => {
-    if (!scriptLoading) {
-      // && window.* provider script
-      context.dispatch({
-        type: SET_SRC_LOADED,
-      });
-      // TODO:
-      // initialize provider
-      // attach lifecycle listeners
-    }
-  }, [scriptLoading]);
+  useProviderScript();
 
   // end hooks
 
