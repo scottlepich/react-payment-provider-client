a7afda2a6fbadd4b13ee6f83b55bd0847aea5b16
Scott Lepich <slepich@opentable.com>
2023-08-03 20:52:30 -0700

    simplify context

--- src/usePayment.ts
+++ src/usePayment.ts
@@ -1,59 +1,17 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
-import useScript from "react-script-hook";
 import PaymentContext from "./PaymentContext";
 
-import useProvider from "./useProvider";
-
-import { PaymentType } from "./constants";
-import { ActionTypes } from "./types/index.d";
-
-// TODO: Stripe
-
-const { SET_SRC_LOADED, SET_ERRORS } = ActionTypes;
-
-const usePayment = (paymentType: PaymentType) => {
+const usePayment = () => {
   const context = useContext(PaymentContext);
 
-  // Get provider specific config
-  const { src, hasWindowModule, initialize, attachEvents } =
-    useProvider(paymentType);
-
-  // Attach provider script
-  const [scriptLoading] = useScript({
-    src,
-    checkForExisting: true, // prevent multiple script injection
-  });
-
-  // Init provider
-  useEffect(() => {
-    if (context && hasWindowModule) {
-      const { dispatch } = context;
-      dispatch({
-        type: SET_SRC_LOADED,
-      });
-      initialize();
-      attachEvents(context);
-    }
-  }, [scriptLoading, context]);
-
-  // must be after hooks:
   if (context === undefined) {
     throw new Error(
       "usePayment hook must be used within a PaymentProvider context.",
     );
   }
 
-  // TODO: callbacks?
-  // TODO: dispatch?
-  const clearErrors = (dispatch: any) => {
-    dispatch({
-      type: SET_ERRORS,
-      error: undefined,
-    });
-  };
-
-  return { ...context, clearErrors };
+  return context;
 };
 
 export default usePayment;
