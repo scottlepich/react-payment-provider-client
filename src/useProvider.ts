6599a74b0cd719850414e674dd4cac5e8849cf93
Scott Lepich <slepich@opentable.com>
2023-08-03 17:34:30 -0700

    use switch for provider

--- src/useProvider.ts
+++ src/useProvider.ts
@@ -4,16 +4,17 @@ import { SPREEDLY_SCRIPT_URL } from "./spreedly/constants";
 // TODO: import { STRIPE_SCRIPT_URL } from "./stripe/constants";
 
 const useProvider = (paymentType: PaymentType) => {
-  if (!Object.values(PaymentType).includes(paymentType)) {
-    throw new Error("Unknown PaymentType passed to usePayment.");
+  switch (paymentType) {
+    case PaymentType.Spreedly:
+      // TODO: moar params
+      return {
+        src: SPREEDLY_SCRIPT_URL,
+        hasWindowModule: typeof window?.Spreedly !== "undefined",
+      };
+    // TODO: case PaymentType.Stripe:
+    default:
+      throw new Error("Unknown PaymentType passed to usePayment.");
   }
-
-  // TODO: get constants based on provider
-  // TODO: moar params
-
-  return {
-    src: SPREEDLY_SCRIPT_URL,
-  };
 };
 
 export default useProvider;
