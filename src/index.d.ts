e1f709c6a8990252ec1895f0b44a775febffc58d
Scott Lepich <slepich@opentable.com>
2023-08-04 19:04:49 -0700

    types seem happier with this simple solution

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,13 @@
 declare module "@opentable/react-payment-provider-client";
 
+import PaymentContextProvider from "./context/PaymentContextProvider";
+
+import usePayment from "./usePayment";
+
+import { CVVField, CardNumberField } from "./iframeNodes";
+
+export { usePayment, PaymentContextProvider, CVVField, CardNumberField };
+
 export type * from "./types";
 
 export * from "./constants";
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
 import PaymentContextProvider from "./context/PaymentContextProvider";
 
-import reducer from "./context/reducer";
-
 import usePayment from "./usePayment";
-
-import { PaymentType } from "./constants";
 
 import { CVVField, CardNumberField } from "./iframeNodes";
 
-export {
-  usePayment,
-  PaymentType,
-  CardNumberField,
-  CVVField,
-  PaymentContextProvider,
-  reducer,
-};
+export { usePayment, PaymentContextProvider, CVVField, CardNumberField };
