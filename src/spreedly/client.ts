f77c39ad761e1e360da3b4a8063dd11a6ca08769
Scott Lepich <slepich@opentable.com>
2023-08-10 10:15:44 -0700

    use full window obj in client

--- src/spreedly/client.ts
+++ src/spreedly/client.ts
@@ -17,14 +17,12 @@ const { SET_3DS_EVENTS, SET_CREDIT_CARD, SET_ERRORS, SET_INPUTS, SET_READY } =
 const { READY, ERRORS, PAYMENT_METHOD, INPUT, THREEDS_STATUS } = SpreedlyEvents;
 
 // TODO: figure out where to store env keys
-const environmentKey = process.env.SPREEDLY_DEMO || "";
-
-// TODO: figure out isomorphic app
-const Spreedly = typeof window !== "undefined" ? window?.Spreedly || {} : {};
+const SPREEDLY_DEMO = "8b01Y9b9sSRkMsbw14aOya8YmQj";
+const environmentKey = SPREEDLY_DEMO;
 
 export const initializeSpreedly = () => {
-  if (Spreedly) {
-    Spreedly.init(environmentKey, {
+  if (window.Spreedly) {
+    window.Spreedly.init(environmentKey, {
       numberEl: CARD_NUMBER_FIELD_ID,
       cvvEl: CVV_FIELD_ID,
     });
@@ -32,31 +30,34 @@ export const initializeSpreedly = () => {
 };
 
 export const attachEvents = ({ state, dispatch }: any) => {
-  if (Spreedly) {
-    Spreedly.on(READY, () => {
+  if (window.Spreedly) {
+    window.Spreedly.on(READY, () => {
       dispatch({
         type: SET_READY,
       });
     });
 
-    Spreedly.on(ERRORS, (error: Error) => {
+    window.Spreedly.on(ERRORS, (error: Error) => {
       dispatch({
         type: SET_ERRORS,
         error,
       });
     });
 
-    Spreedly.on(PAYMENT_METHOD, (token: string, data: CreditCardData) => {
-      dispatch({
-        type: SET_CREDIT_CARD,
-        card: {
-          token,
-          data,
-        },
-      });
-    });
+    window.Spreedly.on(
+      PAYMENT_METHOD,
+      (token: string, data: CreditCardData) => {
+        dispatch({
+          type: SET_CREDIT_CARD,
+          card: {
+            token,
+            data,
+          },
+        });
+      },
+    );
 
-    Spreedly.on(INPUT, (name: string, value: string) => {
+    window.Spreedly.on(INPUT, (name: string, value: string) => {
       dispatch({
         type: SET_INPUTS,
         inputs: [...state.inputs, { name, value }],
@@ -64,7 +65,7 @@ export const attachEvents = ({ state, dispatch }: any) => {
     });
 
     // TODO: 3ds status data type
-    Spreedly.on(THREEDS_STATUS, (data: any) => {
+    window.Spreedly.on(THREEDS_STATUS, (data: any) => {
       dispatch({
         type: SET_3DS_EVENTS,
         threeDsEvents: [{ name: data.event, data }, ...state.threeDsEvents],
@@ -74,8 +75,8 @@ export const attachEvents = ({ state, dispatch }: any) => {
 };
 
 export const tokenizeCard = (creditCard: any) => {
-  if (Spreedly) {
-    Spreedly.tokenizeCreditCard(creditCard);
+  if (window.Spreedly) {
+    window.Spreedly.tokenizeCreditCard(creditCard);
   }
 };
 
@@ -83,8 +84,8 @@ export const startThreeDS = (
   transactionToken: string,
   threeDSLifecycle: any,
 ) => {
-  if (Spreedly) {
-    threeDSLifecycle.current = new Spreedly.ThreeDS.Lifecycle({
+  if (window.Spreedly) {
+    threeDSLifecycle.current = new window.Spreedly.ThreeDS.Lifecycle({
       environmentKey,
       hiddenIframeLocation: HIDDEN_IFRAME_ID,
       challengeIframeLocation: CHALLENGE_IFRAME_ID,
